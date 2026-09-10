9d2d51a67ad42f6ab1b38736f1da9797e0a74287
tamersharbatly-web <tamersharbatly@gmail.com>
2026-09-10 10:00:25 +0300

    Initial deploy for kml analyzer app

---
+++ cdn/1/widgets/common/table/dist/runtime/translations/default.ts
@@ -0,0 +1,19 @@
+export default {
+  _widgetLabel: 'Table',
+  _widgetDescription: 'This is an interactive table widget',
+  showAllRecords: 'Show all records',
+  showHideCols: 'Show/hide columns',
+  toggleSelect: 'Show selection/all',
+  _action_viewInTable_label: 'View in table',
+  tableDataActionLabel: '{layer} table selection',
+  tableCount: 'Total: {total} | Selection: {selected}',
+  _action_addToTable_label: 'Add to table',
+  deleteRecords: 'Delete selection',
+  enableMapExtentFilter: 'Filter by map extent',
+  openSearch: 'Open search',
+  closeSearch: 'Close search',
+  noSelectionTips: 'No features are selected in {layer}.',
+  noColumnsTips: 'No columns are visible. Toggle on one or more columns to view the data.',
+  selectSheet: 'Select a sheet to display',
+  noLayersAvailable: 'No layers or tables are currently available to display.'
+}
